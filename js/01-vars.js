ba6f529257d21480864655d88f76d275ebbb38a5
Taya <taiska1488@gmail.com>
2024-02-11 16:00:35 +0200

    Summary

--- js/01-vars.js
+++ js/01-vars.js
@@ -1,12 +1,12 @@
-const age = 10;
-const totalPrice = 200.74;
-const userName = 'Chelsy';
-const message ='Вітаємо!';
-const isOpen = true;
-const shouldConfirm = false;
+// const age = 10;
+// const totalPrice = 200.74;
+// const userName = 'Chelsy';
+// const message ='Вітаємо!';
+// const isOpen = true;
+// const shouldConfirm = false;
 
-const type = typeof 5;
-console.log(type);
+// const type = typeof 5;
+// console.log(type);
 
 // Як window.alert() ,блокує інтерпретації
 // console.log('before')
---
+++ js/02-vars.js
@@ -0,0 +1,12 @@
+// методи window.confirm і windom.promt()
+
+
+// const shouldRenew = confirm('Бажаете продовжити підписку?') ;
+// console.log(shouldRenew);
+
+// метод Prompt
+
+// let quantity = prompt('Введіть кількість товарів');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
