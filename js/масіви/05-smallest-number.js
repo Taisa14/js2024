601159fd6f8d4a5f69185125a7984bc9cd564a68
Taya <taiska1488@gmail.com>
2024-02-19 18:03:42 +0200

    Update 05-smallest-number.js

--- js/масіви/05-smallest-number.js
+++ js/масіви/05-smallest-number.js
@@ -1,15 +1,31 @@
 // Напиши скрипт поиска самого маленького числа в массиве
 // при условии что числа уникальные не повторяются
 
-const numbers = [51, 18, 13, 24, 7, 85, 19];
-let smallestNumber = numbers[0];
+// const numbers = [51, 18, 13, 24, 7, 85, 19];
+// let smallestNumber = numbers[0];
 
-console.log(smallestNumber);
+// console.log(smallestNumber);
 
-for(const number of numbers) {
-    // console.log(number);
-    if (number < smallestNumber){
-        smallestNumber = number;
-    }
-}
-console.log('smallesNumber: ', smallestNumber);
+// for(const number of numbers) {
+//     // console.log(number);
+//     if (number < smallestNumber){
+//         smallestNumber = number;
+//     }
+// }
+// console.log('smallesNumber: ', smallestNumber);
+
+
+// поиск большего числа
+
+// const numbers = [51, 18, 13, 24, 7, 85, 19];
+// let bigestNumber = numbers[0];
+
+// // console.log(bigestNumber);
+
+// for(const number of numbers) {
+//     // console.log(number);
+//     if (number > bigestNumber){
+//         bigestNumber = number;
+//     }
+// }
+// console.log('bigestNumber: ', bigestNumber);
