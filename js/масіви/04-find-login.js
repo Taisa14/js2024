78fc5d441e3ebecb62b80ea8427ddacd6346a9c2
Taya <taiska1488@gmail.com>
2024-02-19 17:12:04 +0200

    Summary

--- js/масіви/04-find-login.js
+++ js/масіви/04-find-login.js
@@ -9,23 +9,47 @@
 // метод includes() с тернарным оператором
 
 
-const logins = ['love', 'peace', 'world', 'Ukraine'];
-const loginToFind = 'world';
-let message = '';
+// const logins = ['love', 'peace', 'world', 'Ukraine'];
+// const loginToFind = 'world';
+// let message = '';
 
-for (let i = 0; i < loginToFind.length; i += 1) {
-    const login = logins[i];
-    console.log('Login: ', login);
+// for (let i = 0; i < loginToFind.length; i += 1) {
+//     const login = logins[i];
+//     console.log('Login: ', login);
 
-    if ( login === loginToFind) {
-        message = `Пользователь ${loginToFind} найден`;
-        break;
-    }
+//     if ( login === loginToFind) {
+//         message = `Пользователь ${loginToFind} найден`;
+//         break;
+//     }
 
 
-        message = `Пользователь ${loginToFind} не найден`;
+//         message = `Пользователь ${loginToFind} не найден`;
 
     
-}
+// }
 
-console.log(message);
+// console.log(message);
+
+
+// // с методом includes( используем в случае когда нужно 
+// проверить есть ли там такое или нет)
+// const logins = ['love', 'peace', 'world', 'Ukraine'];
+// const loginToFind = 'world';
+// let message = '';
+
+// console.log(logins.includes(loginToFind));
+
+
+// можно декларативным кодом через тернарный оператор
+
+// самое лучшее решение вот это
+
+// const logins = ['love', 'peace', 'world', 'Ukraine'];
+// const loginToFind = 'world';
+
+
+// const message = logins.includes(loginToFind)
+// ? `Пользователь ${loginToFind} найден.`
+// : `Пользователь ${loginToFind} не найден.`;
+
+// console.log(message);
