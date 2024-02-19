e9c4bf32e0fc91b70adf73373a8806aa606379f5
Taya <taiska1488@gmail.com>
2024-02-19 14:31:35 +0200

    Update 01-basics.js

--- js/масіви/01-basics.js
+++ js/масіви/01-basics.js
@@ -1,14 +1,49 @@
 // 'Mango', 'Kiwi', 'Poly', 'Ajax'
 
-const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'] ;
-console.table(friends);
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'] ;
+// console.table(friends);
 
-const lastIndex = friends.length -1;
-console.log(lastIndex);
+// console.log(friends[2]); 
+// обращение к масиву
+
+
+// friends[1] = 'love'
+// friends[3] = 123
+// console.table(friends);
+// // изменение масива
+
+
+// const lastIndex = friends.length -1;
+// console.log(lastIndex);
+// последний масив
 
 // Передача по ссилке та по значенію
 
 // Прімітіви та сложні тіпи 
+
+// let a = 10;
+// let b = a;
+
+
+// console.log(a);
+// console.log(b);
+
+// ссылочный тип сравнения(сравнивается по адрессу) сложный тип
+
+// const a = [1, 2, 3];
+// const b = a;
+
+// console.log('a', a);
+// console.log('b', b)
+
+// a[0] = 500;
+
+// console.log('a', a);
+// console.log('b', b);
+
+// console.log(a === b);
+// console.log([1, 2, 3] === [1, 2, 3]);
+
 
 // Ссылочное равенство (referential equality);
 
@@ -18,4 +53,22 @@ console.log(lastIndex);
 
 
 
-// const friends = ;
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+// console.log(friends);
+// const lastIndex = friends.length -1;
+
+// for(let i = 0; i <lastIndex; i +=1) {
+//     console.log(friends[i]);
+//     // переберется весь масив от нуля и до последнего индекса
+//     // (friends[i]) - это обращение к каждому элементу массива на итерации
+// }
+
+
+const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+console.log(friends);
+const lastIndex = friends.length -1;
+
+for(let i = 0; i <=lastIndex; i += 1) {
+    friends[i] += '-1';
+}
+console.table(friends);
