2c868e91b12d1fb7a6b90493b104d8ce4fda2332
Taya <taiska1488@gmail.com>
2024-02-19 12:43:35 +0200

    Summary

---
+++ js/масіви/01-basics.js
@@ -0,0 +1,21 @@
+// 'Mango', 'Kiwi', 'Poly', 'Ajax'
+
+const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'] ;
+console.table(friends);
+
+const lastIndex = friends.length -1;
+console.log(lastIndex);
+
+// Передача по ссилке та по значенію
+
+// Прімітіви та сложні тіпи 
+
+// Ссылочное равенство (referential equality);
+
+// Перебор (итерация) масива
+// - for если  нужен индекс или нужно изменить элемент масива
+// - for... of если индекс не нужен и в масиве ничего менять не нужно
+
+
+
+// const friends = ;
--- js/модуль 2/dom.js
+++ js/модуль 2/dom.js
@@ -1,25 +1,25 @@
-const btnAdd = document.querySelector('button[data-add]');
-const resetBtn = document.querySelector('button[data-reset]')
-const valueInput = document.querySelector('input[data-value]')
-const outputEl = document.querySelector('.js-output span');
+// const btnAdd = document.querySelector('button[data-add]');
+// const resetBtn = document.querySelector('button[data-reset]')
+// const valueInput = document.querySelector('input[data-value]')
+// const outputEl = document.querySelector('.js-output span');
 
-let total = 0;
-
-
-
-btnAdd.addEventListener('click', function(){
-    console.log('click hehehe');
-    const value = Number(valueInput.value);
-
-    console.log(value);
-    total += value;
-    outputEl.textContent = total;
-    valueInput.value = '';
+// let total = 0;
 
 
-});
 
-resetBtn.addEventListener('click', function () {
-    total = 0;
-    outputEl.textContent = total;
-})
+// btnAdd.addEventListener('click', function(){
+//     console.log('click hehehe');
+//     const value = Number(valueInput.value);
+
+//     console.log(value);
+//     total += value;
+//     outputEl.textContent = total;
+//     valueInput.value = '';
+
+
+// });
+
+// resetBtn.addEventListener('click', function () {
+//     total = 0;
+//     outputEl.textContent = total;
+// })
