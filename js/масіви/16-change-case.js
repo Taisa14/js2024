50ddb19f84157630f2d00e2d229fc2580a7c7a76
Taya <taiska1488@gmail.com>
2024-02-20 20:16:33 +0200

    Summary

---
+++ js/масіви/16-change-case.js
@@ -0,0 +1,34 @@
+// Напиши функцию changeCase(string) которая заменяет регистр 
+// каждого символа в строке на противоположный
+// Например если строка "JavaScript", то на выходе должна быть строка "jAVAsCRIPT".
+
+// const string = 'JavaScript';
+
+
+
+
+
+
+// 
+
+
+// console.log('invertedString: ', invertedString);
+
+const changeCase = function (string){
+    const letters = string.split('');
+    let invertedString = '';
+
+    for (const letter of letters) {
+        
+        const isInLowerCase = letter === letter.toLowerCase();
+         invertedString += isInLowerCase ? letter.toUpperCase() : letter.toLowerCase();
+        }
+
+
+    return invertedString;
+
+}
+
+console.log(changeCase('JavaScript'));
+console.log(changeCase('mAngO'));
+console.log(changeCase('POlly'));
