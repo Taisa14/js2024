00b463b01b80ea0a95de9ddfd9ec929989ae39c5
Taya <taiska1488@gmail.com>
2024-02-23 17:54:35 +0200

    Update-1 creation

--- js/об'єкти/01-creation.js
+++ js/об'єкти/01-creation.js
@@ -3,15 +3,56 @@
 // - Свойства, ключи(имя) и значение
 // - Как отличить объект от области видимости
 
+// const playlist = {
+//     name: 'Мой супер плейлист',
+//     ratings: 5,
+//     tracks: ['трек-1', 'трек-2', 'трек-3'],
+//     trackCount: 3,
+// };
+// console.log(playlist);
+
+
+
+// Пример литерал объекта
+
+// const x = {};
+// const fn = function (myObject) {
+//     myObject = {a: 1, b: 2}
+//     console.log(myObject);
+// }
+
+// fn({a: 1, b: 2 });
+
+// Литерал возвращение в результате присваевается куда-то
+
+// const rtfm = function () {
+   
+//     return {a: 5};
+// }
+// console.log(rtfm);
+
+
 const playlist = {
     name: 'Мой супер плейлист',
     ratings: 5,
     tracks: ['трек-1', 'трек-2', 'трек-3'],
     trackCount: 3,
 };
-console.log(playlist);
+// console.log(playlist.tracks);
+// console.log(playlist.name);
 
 
+const propertyName = 'tracks';
+
+// console.log(playlist['ratings']);
+// console.log(playlist.ratings);
+
+// console.log(playlist.propertyName);
+// console.log(playlist[propertyName]);
+
+// Если нам нужно обратиться к свойству мы ставим точку и обращаемся
+
+// к оъекту. console.log(playlist.tracks);
 // объект это как словарь возможноть  сгрупировать характеристики какой-то одной сущности
 
 // МАСИВЫ мы используем чтобы хранить что-то внем(коллекцию), 
@@ -27,10 +68,15 @@ console.log(playlist);
 
 // Короткая запись свойства
 
-// const username = 'Mango';
-// const email = 'mango@gmail.com';
+const username = 'Mango';
+const email = 'mango@gmail.com';
 
-// const signupDate = {};
+const signupDate = {
+    username: username,
+    email: email,
+};
+
+console.log(signupDate);
 
 // // Вычисляем свойства
 
