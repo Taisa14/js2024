a5bb529e5ea27d4ad803e80f89d581e0f26d8c8f
Taya <taiska1488@gmail.com>
2024-02-27 20:43:57 +0200

    update

--- js/об'єкти/05-cart.js
+++ js/об'єкти/05-cart.js
@@ -20,7 +20,27 @@ const cart = {
         return this.items;
     },
     add(product) {
-        this.items.push(product);
+
+        for(const item of this.items) {
+            // console.log(item);
+        if(item.name === product.name) {
+
+       item.quantity += 1;
+       return;
+      }
+
+        }
+
+
+
+
+        const newProduct = {
+            ...product,
+            quantity: 1,
+        };
+        
+
+        this.items.push(newProduct);
     },
     remove(productName) {
         const { items } = this;
@@ -40,20 +60,43 @@ for (let i = 0; i < items.length; i+= 1) {
 
         
     },
-    clear() {},
-    countTotalPrice() {},
+    clear() {
+        this.items = []
+    },
+    countTotalPrice() {
+        
+        const { items } = this;
+        let total = 0;
+
+        for (const { price, quantity } of items) {
+            total += price * quantity;
+        }
+        return total;
+
+
+
+    },
     increaseQuantity(productName) {},
     decreaseQuantity(productName) {},
 };
 
-console.log(cart.getItems());
+// console.log(cart.getItems());
 
 cart.add({name: 'apple', price: 50});
 cart.add({name: 'strawberry', price: 70});
 cart.add({name: 'limon', price: 60});
 cart.add({name: 'tomato', price: 110});
+cart.add({name: 'tomato', price: 110});
+cart.add({name: 'tomato', price: 110});
 
 console.table(cart.getItems());
+
+console.log('Total: ', cart.countTotalPrice());
 
 cart.remove ('limon');
 console.table(cart.getItems());
+
+cart.clear();
+console.log(cart.getItems());
+
+
