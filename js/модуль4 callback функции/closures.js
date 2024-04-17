1cb1535627a9fcb64143b8c33355427203b9bf2a
Taya <taiska1488@gmail.com>
2024-04-17 22:02:25 +0300

    Update

--- js/модуль4 callback функции/closures.js
+++ js/модуль4 callback функции/closures.js
@@ -27,21 +27,57 @@
 // Поваренок
 
 
-const makeSheff = function (name) {
+// const makeSheff = function (name) {
 
-    const makeDish = function (dish) {
-        console.log(`${name} готовит ${dish}`);
-    };
-    return makeDish;
-};
+//     const makeDish = function (dish) {
+//         console.log(`${name} готовит ${dish}`);
+//     };
+//     return makeDish;
+// };
 
-const mango = makeSheff('Mango');
+// const mango = makeSheff('Mango');
 
-mango('котлеты');
-mango('пирожок');
+// mango('котлеты');
+// mango('пирожок');
 
-const poly = makeSheff('Poly');
+// const poly = makeSheff('Poly');
 
-poly('чай');
-poly('омлет');
+// poly('чай');
+// poly('омлет');
 
+// Округлятор
+// const rounder = function (places) {
+//     return function (number) {
+//         return Number(number.toFixed(places));
+//     };
+// };
+
+// const rounder2 = rounder(2);
+// const rounder3 = rounder(3);
+
+// console.log(rounder2(3.4567));
+// console.log(rounder3(3.4567));
+// console.log(rounder2(5.1234));
+// console.log(rounder3(3.4567));
+
+
+// Приватные данные и функции - скрытие реализации, интерфейс
+
+// const salaryManagerFactory = function (employeeName, baseSalary = 0) {
+//     let salary = baseSalary;
+
+//     return {
+//         raise(amount) {
+//             salary += amount;
+//         },
+//         lower(amount) {
+//             salary -= amount;
+//         },
+//         current() {
+//             return `Текущая зарплата ${employeeName} - ${salary}`;
+//         },
+//     };
+// };
+
+// const salaryManager = salaryManagerFactory('Mango', 6000);
+// console.log(salaryManager.current());
