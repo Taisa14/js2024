276be69873595d362b645be1f570fcd6fb73e4a4
Taya <taiska1488@gmail.com>
2024-04-22 20:55:46 +0300

    filter

--- js/модуль4 callback функции/Перебирающие методы массивов.js
+++ js/модуль4 callback функции/Перебирающие методы массивов.js
@@ -45,13 +45,13 @@ const players = [
 
 // Получаем массив имен всех игроков
 
-const playerNames = players.map(player => player.name)
-    console.log('playerNames', playerNames);
+// const playerNames = players.map(player => player.name)
+//     console.log('playerNames', playerNames);
 
 
 
-const playerIds = players.map(player => player.id)
-console.log('playerIds', playerIds);
+// const playerIds = players.map(player => player.id)
+// console.log('playerIds', playerIds);
 
 // const res = players.map(player => {
 //     return{
@@ -100,3 +100,18 @@ console.log('playerIds', playerIds);
 //     : player,
 // );
 // console.table(updatedPlayers);
+
+
+        //    Фильтр
+        - Array.prototype.filter()
+        
+
+        const numbers = [5, 10, 15, 20, 25];
+
+        const players = [
+            {id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false},
+            {id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true},
+            {id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true},
+            {id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false},
+            {id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true},
+        ];
