c0ef2a8754d6944a22912190cfed9d7620c59474
Taya <taiska1488@gmail.com>
2024-04-22 12:28:31 +0300

    Foreach

---
+++ js/модуль4 callback функции/Перебирающие методы массивов.js
@@ -0,0 +1,18 @@
+// foreach.js
+
+// Array.prototype.forEach((currentValue, index, array), thisArg);
+// - Поэлементно перебирает оригинальный масив 
+// - ничего не возвращает
+// -заменяет классический for, если не нужно прерывать цикл
+
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// numbers.forEach(function(number, index,array) {
+//     console.log('number', number);
+    
+// });
+
+// console.log(numbers);
+
+
